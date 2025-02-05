0cd3f78cbfa19253b5732f97055da4f04790e627
CSEONS <ashabsadaev360@gmail.com>
2025-02-05 13:15:59 +0300

    .

--- ReverseLayout/SettingsForm.Designer.cs
+++ ReverseLayout/SettingsForm.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            checkBox1 = new CheckBox();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(12, 62);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(261, 19);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Запускать в фоновом режиме при запуске";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(696, 415);
+            button1.Name = "button1";
+            button1.Size = new Size(92, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Применить";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 12);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 2;
+            comboBox1.Text = "Language";
+            // 
+            // SettingsForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(comboBox1);
+            Controls.Add(button1);
+            Controls.Add(checkBox1);
+            Name = "SettingsForm";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private CheckBox checkBox1;
+        private Button button1;
+        private ComboBox comboBox1;
     }
 }
